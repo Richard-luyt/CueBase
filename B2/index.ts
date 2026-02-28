6b22d3d3be872c1ef4b143c637117d1c20a27f1b
Lyt <luyetian215@gmail.com>
2026-02-27 22:34:19 -0500

    bug fixed

--- B2/index.ts
+++ B2/index.ts
@@ -36,7 +36,7 @@ app.use(cors({
     'https://www.cuebase.online',
     'http://localhost:3000',
     'http://localhost:5173',
-  ];,
+  ],
   credentials: true,
 }));
 app.use(express.json({ limit: "10kb" }));
